24c32a9c1e6fa732bf0d3b7d4bab09259bbe1040
hossein <hosseinbakhtiari781@outlook.com>
2024-05-08 21:29:52 +0330

    refactoring the nav menu

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -8,45 +8,44 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/16/solid";
 import ButtonLink from "../commen/ButtonLink";
 
 function Navbar() {
+  const textLinkClasses =
+    "text-gray-500 hover:text-gray-900 active:text-gray-400";
+
+  const navLinks = [
+    { href: "#features", children: "Featuers" },
+    { href: "#reviews", children: "Reviews" },
+    { href: "#partners", children: "Partners" },
+  ];
+
   return (
     <Disclosure as="nav">
       {({ open }) => (
         <>
-          <div className="flex h-[15vh] max-w-7xl items-center justify-between px-8">
+          <div className="flex h-[15vh] max-w-7xl items-center justify-between px-8 xl:m-auto lg:px-12">
             <div className="flex">
               <ButtonLink
                 href="#"
                 children={<img src={Kobodrop} alt="Kobodrop logo" />}
               />
               <div className="ml-4 hidden items-center  space-x-4 sm:flex ">
-                <a
-                  className="text-gray-500 hover:text-gray-900 active:text-gray-400"
-                  href="#features"
-                >
-                  Features
-                </a>
-                <a
-                  className="text-gray-500 hover:text-gray-900 active:text-gray-400"
-                  href="#partners"
-                >
-                  Partners
-                </a>
-                <a
-                  className="text-gray-500 hover:text-gray-900 active:text-gray-400"
-                  href="#reviews"
-                >
-                  Reviews
-                </a>
+                {navLinks.map((link, index) => (
+                  <ButtonLink
+                    key={index}
+                    href={link.href}
+                    children={link.children}
+                    className={textLinkClasses}
+                  />
+                ))}
               </div>
             </div>
 
-            <a
+            <ButtonLink
               href="https://play.google.com"
               target="_blank"
+              children={"Install App"}
               className="hidden rounded-xl bg-gray-700 px-5 py-3 text-white hover:bg-gray-900 active:bg-gray-600 sm:flex"
-            >
-              Install App
-            </a>
+            />
+
             <DisclosureButton className="rounded-md p-2 text-gray-500 hover:bg-gray-700 hover:text-white sm:hidden">
               {open ? (
                 <XMarkIcon className="block h-6 w-6" />
@@ -55,16 +54,17 @@ function Navbar() {
               )}
             </DisclosureButton>
           </div>
-          <DisclosurePanel>
-            <DisclosureButton className="block" as="a" href="#features">
-              Features
-            </DisclosureButton>
-            <DisclosureButton className="block" as="a" href="#reviews">
-              Reviews
-            </DisclosureButton>
-            <DisclosureButton className="block" as="a" href="#partners">
-              Partners
-            </DisclosureButton>
+          <DisclosurePanel className="space-y-1 px-4 sm:hidden">
+            {navLinks.map((link, index) => (
+              <DisclosureButton
+                className="block"
+                as="a"
+                href="#features"
+                key={index}
+              >
+                {link.children}
+              </DisclosureButton>
+            ))}
           </DisclosurePanel>
         </>
       )}
